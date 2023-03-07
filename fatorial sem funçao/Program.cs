8a2c0098c07ba8203bcdc186003379717c8f2188
André misuraca <m1sur4k@gmail.com>
2023-03-06 22:07:20 -0300

    Adicionar arquivos de projeto.

---
+++ fatorial sem funçao/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número inteiro: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        int fatorial = Fatorial(numero);
+
+        Console.WriteLine($"O fatorial de {numero} é {fatorial}");
+    }
+
+    static int Fatorial(int num)
+    {
+        if (num == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return num * Fatorial(num - 1);
+        }
+    }
+}
